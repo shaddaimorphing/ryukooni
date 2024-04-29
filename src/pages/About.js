00cb00e57f53bec77ee5ac6ee0a6bae72f617dcf
shaddaimorphing <shaddaimorphing@gmail.com>
2024-04-28 20:55:24 -0700

    Updated About and Install pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import { Stack, Typography } from '@mui/material'
+import { Divider, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const About = () => {
@@ -7,18 +7,61 @@ const About = () => {
             <Typography variant="h1" component="div">
                 Why Install This?
             </Typography>
-
             <Typography variant="h4" component="div">
                 Though SYW is an amazing mod that provides an HD remaster of sorts to FFVII, I believe that being that its options menu is very intimidating, and may turn away some people not used to modding. Not only that, the choices may bring uncertainty in what they should select for their playthrough. This guide is meant to circumvent both of these barriers. Ryuko Oni not only provides the improvements of SYW, but also chooses options on what are the best to retain the accuracy of FFVII. Below is a list of the major options I've selected, and why I beleive they are important.
             </Typography>
 
+            <Divider sx={{bgcolor: "white"}} />
+
             <Typography variant="h2" component="div">
                 HD Backgrounds
             </Typography>
-
             <Typography variant="h4" component="div">
-                HD Backgrounds are the main reason why you install SYW. Upscaled with AI, they provide clearer backgrounds for a visually enhanced experience. Also with ChaOS's extended backgrounds, you get a genuine 16:9 screen. Though artifacts from the AI upscale are visible if you look closely, I believe its vastly worth it.
+                HD Backgrounds are the main reason why you install SYW. Square's original backgrounds were lost, and couldn't be rescanned for rereleases, ending up with the  Upscaled with AI, they provide clearer backgrounds for a visually enhanced experience. Also with ChaOS's extended backgrounds, you get a genuine 16:9 screen. Though artifacts from the AI upscale are visible if you look closely, I still believe its worth it.
             </Typography>
+            
+            <Typography variant="h2" component="div">
+                Ninostyle Field Models
+            </Typography>
+            <Typography variant="h4" component="div">
+                The original models for FFVII were obviously conceived due to the limitations of the hardware of the time. Though that is the case, the game is designed around these models, so scenes will have expressive movements utilizing the cartoonish proportions. So it is only fitting that we keep that intact, and not dramatically change the way they look. Ninostyle's field models modernize these the originals by going for a very stylized approach that fits in with the art direction of the backgrounds. While not every model has been updated, enough has been done where the effects are apparent.
+            </Typography>
+
+            <Typography variant="h2" component="div">
+                Ninostyle Battle Models
+            </Typography>
+            <Typography variant="h4" component="div">
+                While the battle models aren't as much of a result of limitations, they are still worth touch up on. Like before, Ninostyle goes for style, this time emulating Tetsuya Nomura's art for the character portraits in the menus. It creates a nice bridge in between the art and the game models. All models haven't been worked on, but the human ones have, which benefit the most from this mod.
+            </Typography>
+
+            <Typography variant="h2" component="div">
+                HD World Map
+            </Typography>
+            <Typography variant="h4" component="div">
+                Replaces textures for the world map with highter quality ones. Also has QOL improvements regarding the camera
+            </Typography>
+
+            <Typography variant="h2" component="div">
+                HD Menus
+            </Typography>
+            <Typography variant="h4" component="div">
+                Upscales the menus while retaining the design
+            </Typography>
+
+            <Typography variant="h2" component="div">
+                60fps 
+            </Typography>
+            <Typography variant="h4" component="div">
+                Increases the framerate in battle and the field to 60fps. May not seem that important for a turn based game, but it makes the game feel less old, which can turn people away.
+            </Typography>
+
+            <Typography variant="h2" component="div">
+                Original Soundtrack
+            </Typography>
+            <Typography variant="h4" component="div">
+                Replaces the MIDI soundtrack added for the PC release with the higher quality original soundtrack from the PSX release.
+            </Typography>
+
         </Stack>
     )
 }
--- src/pages/Install.js
+++ src/pages/Install.js
@@ -1,4 +1,4 @@
-import { Divider, Stack, Typography } from '@mui/material'
+import { Divider, Link, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const Install = () => {
@@ -15,17 +15,19 @@ const Install = () => {
             <Divider sx={{bgcolor: "white"}} />
 
             <Typography variant="h4" component="div">
-                1. First, download the preset here.
+                1. First, download the preset (06-Ryuko.Oni.ini) from the last release on <Link href="https://github.com/shaddaimorphing/ryukooni/releases">Github</Link>
             </Typography>
             <Typography variant="h4" component="div">
-                2. Then go to where you downloaded the file (usually your Downloads folder) and copy it.
+                2. Then go to where you downloaded the file (usually your Downloads folder) and Rename it to "06-Ryuko Oni.ini".
             </Typography>
             <Typography variant="h4" component="div">
-                3. Navigate to your SYW folder and find the preset folder. Paste the preset there.
+                3. Copy it, and navigate to your SYW folder (Usually C://FF7_SYW_Unified/Presets/English) and find the preset folder. Paste the preset there.
             </Typography>
+            <img src={require('../img/Ryuko Oni Location.png')} alt="" width="75%" />
             <Typography variant="h4" component="div">
                 4. Open SYW and open the dropdown menu. The preset "Ryuko Oni" should be there. Select it.
             </Typography>
+            <img src={require('../img/SYW Menu.png')} alt="" width="75%" />
             <Typography variant="h4" component="div">
                 5. Start the game, and enjoy!
             </Typography>
