fefce1172a9aad1c3eca49672391cdb6c3d2228a
morado9000 <matthewmorado@gmail.com>
2024-04-30 15:38:27 -0700

    Vite version

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import { ThemeProvider, useTheme } from '@mui/material/styles';
@@ -53,23 +54,35 @@ const theme = createTheme({
   }
 });
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "install",
+        element: <Install />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="install" element={<Install />} />
-            <Route path="about" element={<About />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/pages/Install.jsx
+++ src/pages/Install.jsx
@@ -1,5 +1,8 @@
 import { Divider, Link, Stack, Typography } from '@mui/material'
 import React from 'react'
+import ryukoLocation from '../assets/img/Ryuko Oni Location.png'
+import sywMenu from '../assets/img/SYW Menu.png'
+
 
 const Install = () => {
     return (
@@ -23,11 +26,11 @@ const Install = () => {
             <Typography variant="h4" component="div">
                 3. Copy it, and navigate to your SYW folder (Usually C://FF7_SYW_Unified/Presets/English) and find the preset folder. Paste the preset there.
             </Typography>
-            <img src={require('../img/Ryuko Oni Location.png')} alt="" width="75%" />
+            <img src={ryukoLocation} alt="" width="75%" />
             <Typography variant="h4" component="div">
                 4. Open SYW and open the dropdown menu. The preset "Ryuko Oni" should be there. Select it.
             </Typography>
-            <img src={require('../img/SYW Menu.png')} alt="" width="75%" />
+            <img src={sywMenu} alt="" width="75%" />
             <Typography variant="h4" component="div">
                 5. Start the game, and enjoy!
             </Typography>
